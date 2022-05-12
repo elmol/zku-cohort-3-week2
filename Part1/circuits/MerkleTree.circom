2bd7c0c7884dec6c648be544c7ee33bc07cc473c
emolini <emolini@avaya.com>
2022-05-12 19:09:27 -0300

    ZKU Assigment Week 2 - 1

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -1,12 +1,35 @@
 pragma circom 2.0.0;
 
 include "../node_modules/circomlib/circuits/poseidon.circom";
+include "../node_modules/circomlib/circuits/mux1.circom";
 
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
     signal input leaves[2**n];
     signal output root;
 
     //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+  
+    //Initialize the hash nodes: total is 2**n - 1 ( #leaves - 1) (exclude leaves)
+    component hashNodes[ 2**n - 1];
+ 
+    // constraint hashes for leaft hashes nodes: total is 2**n/2  ( #leaves/2 )
+    var  i = 0;
+    for (i=0; i < 2**n/2; i++) {
+        hashNodes[i] = Poseidon(2);
+        hashNodes[i].inputs[0] = leaves[2*i];
+        hashNodes[i].inputs[1] = leaves[2*i+1];
+    }
+
+    // constraint intermediate hashes: total is 2**n - 2 ( #hashers - 1) (exclude leaves hashers)
+    for(i=i; i < 2**n - 2; i++) {
+        var j= (i-2**n/2)*2;
+        hashNodes[i] = Poseidon(2);
+        hashNodes[i].inputs[0] = hashNodes[j].out;
+        hashNodes[i].inputs[1] = hashNodes[j+1].out;
+    }
+
+    // constraint root: last hasher node result (2**n - 1) -1 (#hashers -1) 
+    root <== hashNodes[2**n - 2].out;
 }
 
 template MerkleTreeInclusionProof(n) {
@@ -16,4 +39,29 @@ template MerkleTreeInclusionProof(n) {
     signal output root; // note that this is an OUTPUT signal
 
     //[assignment] insert your code here to compute the root from a leaf and elements along the path
+    signal results[n+1]; // to store the results hashes of the path
+
+    //constraint for all n levels
+    component mux1[n][2];
+    component hash[n];
+    results[0] <== leaf;
+    for (var i = 0; i < n; i++) {
+        mux1[i][0] = Mux1();
+        mux1[i][0].c[0] <== results[i];
+        mux1[i][0].c[1] <== path_elements[i];
+        mux1[i][0].s <== path_index[i];
+        mux1[i][1] = Mux1();
+        mux1[i][1].c[0] <== path_elements[i];
+        mux1[i][1].c[1] <== results[i];
+        mux1[i][1].s <== path_index[i];
+
+        hash[i] = Poseidon(2);
+        hash[i].inputs[0] <== mux1[i][0].out; 
+        hash[i].inputs[1] <== mux1[i][1].out;
+
+        results[i + 1] <== hash[i].out;
+    }
+
+    // constraint for root
+    root <== results[n];
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -3,18 +3,68 @@ pragma solidity ^0.8.0;
 
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
+import "hardhat/console.sol"; //to print logs in the console
 
 contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
 
+    uint256 private constant ZERO_VALUE = 0;
+    uint256 private constant LEVELS = 3;
+
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        uint n = LEVELS; //levels
+        for (uint256 i = 0; i < 2**n; i++) {
+            hashes.push(ZERO_VALUE);
+        }
+
+        uint total = 2**(n+1);
+        for (uint256 level=n; level>0; level--) {
+            uint nodes = 2**level/2;
+            for (uint256 i = 0; i < nodes; i++) {
+                uint before = total - 2**(level+1) ;
+                uint left = 2*i + before;
+                uint right = 2*i + 1 + before;
+                hashes.push(PoseidonT3.poseidon([hashes[left],hashes[right]]));
+                console.log("left: " , left , " right: " , right);
+            }
+        }
+        root = hashes[total-2];
+        
+        console.log("---- initial tree -----");
+        for(uint256 i = 0; i < 15; i++) {
+            console.log(i,":", hashes[i]);
+        }
+        console.log("-----------------------");
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        uint n = LEVELS;
+        require(index < 2**n, "MerkleTree is full");
+        hashes[index] = hashedLeaf;
+        uint position = index; 
+        for (uint i = 0; i < n; i++) { 
+            uint odd = position % 2;
+            uint left = position - odd;
+            uint right = position + 1 - odd;
+            position = 2**n + (position - odd) / 2;
+            hashes[position]=PoseidonT3.poseidon([hashes[left],hashes[right]]);
+            console.log("index", position);
+            console.log("left: " , left , " right: " , right);
+        }
+        root = hashes[2**(n+1)-2];
+        index++;
+        return index-1;
+    }
+
+    function printTree() public view{
+        // [assignment] print the Merkle tree
+        for(uint256 i = 0; i < 15; i++) {
+            console.log(i,":", hashes[i]);
+        }
     }
 
     function verify(
@@ -25,5 +75,8 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        console.log("root input:", input[0]);
+        console.log("root generated:",root);
+        return input[0] == root && this.verifyProof(a, b, c, input);
     }
 }
